2f53d9769d5ea02cdd6fb01b1956442c9f5b4306
Lilianwangechi <lilian.wangechi@student.moringaschool.com>
2021-12-09 22:04:34 +0300

    add html file,readme file and linked js to html

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var add = function(number1,number2,){return number1 + number2;
+};
+var number1 =  parseInt(prompt("Entera number:"));
+var number2 = parseInt(prompt ("Enter another number:"));
+alert(add(number1,number2));
